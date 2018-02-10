b318cd5d717ef5cac371899469ab399061be7eba
mdtw <marcuswaters@Marcuss-MacBook-Air.local>
2018-02-10 11:13:47 -0500

    quest2

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+let toDoList = ['Thing One', 'Thing Two', 'Thing Three']
+
 app.get('/todos', (req, res) => 
-    res.send(JSON.stringify(['Thing One', 'Thing Two', 'Thing Three']  )))
+    res.send(JSON.stringify(toDoList)))
+
+app.post('/addToDo', (req, res) => {
+    toDoList.push("Thing Four") //no need to parse anything here = we are not specifying the req 
+    // specifically for a body that isn't a string etc. Therefore just push the actual string to the array.
+    res.send() 
+})
 
 app.listen(3000, () => console.log('Port 3000 is live!') )
